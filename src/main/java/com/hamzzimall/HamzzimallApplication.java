82978eef34ccd174ae8c663fac93bbe4e87fb18a
kyung <bamtori1029@gmail.com>
2024-11-08 19:28:47 +0900

    first commit

---
+++ src/main/java/com/hamzzimall/HamzzimallApplication.java
@@ -0,0 +1,13 @@
+package com.hamzzimall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamzzimallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamzzimallApplication.class, args);
+	}
+
+}
